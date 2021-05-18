117cad3f49dedb813265e5b273ec89cfcb629864
Gary Hughes <garyedwardhughes@gmail.com>
2021-05-18 21:22:05 +1000

    Customise Pedigree.ToString()

--- Fix.Dictionary/Pedigree.cs
+++ Fix.Dictionary/Pedigree.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections.Generic;
+
 namespace Fix
 {
     public static partial class Dictionary
@@ -11,6 +14,48 @@ namespace Fix
             public string? UpdatedEP { get; init; }
             public string? Deprecated { get; init; }
             public string? DeprecatedEP { get; init; }
+
+            public override string ToString()
+            {
+                var items = new List<String>();
+
+                if (Added is string added)
+                {
+                    items.Add($"Added = {added}");
+                }
+
+                if (AddedEP is string addedEP)
+                {
+                    items.Add($"AddedEP = {addedEP}");
+                }
+
+                if (Updated is string updated)
+                {
+                    items.Add($"Updated = {updated}");
+                }
+
+                if (UpdatedEP is string updatedEP)
+                {
+                    items.Add($"UpdatedEP = {updatedEP}");
+                }
+
+                if (Deprecated is string deprecated)
+                {
+                    items.Add($"Deprecated = {deprecated}");
+                }
+
+                if (DeprecatedEP is string deprecatedEP)
+                {
+                    items.Add($"DeprecatedEP = {deprecatedEP}");
+                }
+
+                if (items.Count == 0)
+                {
+                    return string.Empty;
+                }
+
+                return string.Join(", ", items);
+            }
         }
     }
 }
