320e7f52b4c2c21d2bbffdcb0c3bba1585ed3451
Gary Hughes <garyedwardhughes@gmail.com>
2021-06-05 10:08:50 +1000

    Skeleton example program for Fix.Dictionary usage.

---
+++ Examples/Fix.Dictionary.Api/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using static Fix.Dictionary;
+
+namespace Fix.Examples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EnumerateFixVersions();
+        }
+
+        static void EnumerateFixVersions()
+        {
+            foreach (var version in Versions)
+            {
+                Console.WriteLine(version.BeginString);
+            }
+        }
+
+    }
+}
